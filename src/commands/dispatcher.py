bdcf65a46f2b5712ba9693b30a03d9e3edca1fbe
Alexander Vasilyev <natsukashii_nane@proton.me>
2025-03-18 15:25:40 +0200

    help command added

--- src/commands/dispatcher.py
+++ src/commands/dispatcher.py
@@ -1,7 +1,9 @@
+from commands.help import handle_help
 from commands.like import handle_like
 from commands.top import handle_top
 
 COMMANDS = {
+    "help": (handle_help, "Выводит список доступных команд и их описание."),
     "top": (handle_top, "Показывает топ пользователей по лайкам."),
     "like": (handle_like, "Ставит лайк сообщению, на которое вы ответили."),
 }
