cbc9c39dc2b66416a9730c19dd33eabd2e100361
Vasiniyo <sdurdom2228@gmail.com>
2025-03-24 13:29:59 +0700

    Added command hints

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import logging
 
-from commands.dispatcher import handlers
+from telebot.types import BotCommand
+
+from commands.dispatcher import COMMANDS, handlers
 from config import bot
 
 logger = logging.getLogger(__name__)
@@ -9,5 +11,6 @@ if __name__ == "__main__":
     logger.info("Bot started")
     for handler, args in handlers.items():
         bot.message_handler(**args)(handler)
+    bot.set_my_commands([BotCommand(cmd, desc[1]) for cmd, desc in COMMANDS.items()])
     bot.delete_webhook(drop_pending_updates=True)
     bot.polling()
