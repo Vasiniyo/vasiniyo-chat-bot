e517cb5d47fd6d98b720ed9b526e6f2e4dbf8587
Gilenor <beldur7@gmail.com>
2025-04-08 11:33:17 +0300

    added saving of admin custom_title to database

--- src/database/titles.py
+++ src/database/titles.py
@@ -1,14 +1,14 @@
-from database.utils import commit_query, fetch_number
+from database.utils import commit_query, fetch_number, is_column_exist
 
 
 def is_day_passed(chat_id, user_id):
     return fetch_number(
         """
-       select date('now', 'localtime') > date(last_changing, 'unixepoch', 'localtime')
-       from titles
-       where chat_id = ?
-       and user_id = ?
-    """,
+        select date('now', 'localtime') > date(last_changing, 'unixepoch', 'localtime')
+        from titles
+        where chat_id = ?
+        and user_id = ?
+        """,
         (chat_id, user_id),
     )
 
@@ -17,10 +17,39 @@ def commit_dice_roll(chat_id, user_id):
     commit_query(
         """
         insert into titles (chat_id, user_id, last_changing) values (?, ?, 0)
-        on conflict (chat_id, user_id) do update set last_changing = strftime('%s', 'now');
+        on conflict (chat_id, user_id) do update set 
+        last_changing = strftime('%s', 'now');
         """,
         (chat_id, user_id),
     )
+
+
+def commit_update_title(chat_id, user_id, user_title):
+    commit_query(
+        """
+        insert into titles (chat_id, user_id, user_title, last_changing) values (?, ?, ?, 0)
+        on conflict (chat_id, user_id) do update set 
+        last_changing = strftime('%s', 'now'),
+        user_title = excluded.user_title;
+        """,
+        (chat_id, user_id, user_title),
+    )
+
+
+def get_user_title(chat_id, user_id):
+    return fetch_number(
+        """
+        select user_title
+        from titles
+        where chat_id = ?
+        and user_id = ?
+        """,
+        (chat_id, user_id),
+    )
+
+
+def is_user_has_title(chat_id, user_id):
+    return get_user_title(chat_id, user_id) is not None
 
 
 commit_query(
@@ -34,3 +63,13 @@ commit_query(
     """,
     (),
 )
+
+
+if not is_column_exist("titles", "user_title"):
+    commit_query(
+        """
+        alter table titles
+        add column user_title text
+        """,
+        (),
+    )
