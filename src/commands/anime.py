f3c449b092e424af7c2c3b5992df63a9921b67ce
Artem Ovsyannikov <artemrotfront@gmail.com>
2026-02-01 07:55:18 +0300

    Fix anime api

--- src/commands/anime.py
+++ src/commands/anime.py
@@ -1,17 +1,29 @@
+import random
+
 import requests
 
 import safely_bot_utils as bot
 
 
 def handle_anime(message):
-    response = requests.get(
-        "https://shikimori.one/api/animes",
-        params={"order": "random", "score": 8, "limit": 1},
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-        },
+    query = """
+    query ($page: Int) {
+        Page(page: $page, perPage: 1) {
+            media(type: ANIME, sort: POPULARITY_DESC, averageScore_greater: 75, isAdult: false) {
+                siteUrl
+            }
+        }
+    }
+    """
+
+    response = requests.post(
+        "https://graphql.anilist.co",
+        json={"query": query, "variables": {"page": random.randint(1, 50)}},
+        headers={"Content-Type": "application/json"},
     )
+
     if response.status_code == 200:
-        bot.reply_to(f"https://shikimori.one{response.json()[0]['url']}")(message)
+        anime = response.json()["data"]["Page"]["media"][0]
+        bot.reply_to(anime["siteUrl"])(message)
     else:
         bot.reply_to(bot.phrases("anime_failed"))(message)
