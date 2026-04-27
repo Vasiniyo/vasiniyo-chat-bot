1efc624efc9db7983be051269060d4fe8e6d0e3a
Vasiniyo <sdurdom2228@gmail.com>
2026-04-27 22:40:45 +0700

    feat: add gender-aware custom titles

--- src/vasiniyo_chat_bot/module/titles/titles_provider.py
+++ src/vasiniyo_chat_bot/module/titles/titles_provider.py
@@ -7,11 +7,40 @@ class TitlesProvider:
     def __init__(self, custom_titles: CustomTitles):
         self._adjectives = custom_titles.adjectives
         self._nouns = custom_titles.nouns
-        self._weights = custom_titles.weights
 
     def next_title(self):
-        adj = random.choices(self._adjectives, weights=self._weights, k=1)[0]
-        noun = random.choice(
-            [noun for noun in self._nouns if len(adj) + 1 + len(noun) <= 16]
-        )
-        return f"{adj} {noun}"
+        adj_group = random.choice(self._adjectives)
+        adj_base = random.choice(adj_group.base)
+        match random.randint(0, 3):
+            case 0:
+                adj = f"{adj_base}{adj_group.male_ending}"
+                noun_group = random.choice(self._nouns.male)
+                noun_base = random.choice(noun_group.base)
+                noun = f"{noun_base}{noun_group.singular_ending}"
+            case 1:
+                adj = f"{adj_base}{adj_group.female_ending}"
+                noun_group = random.choice(self._nouns.female)
+                noun_base = random.choice(noun_group.base)
+                noun = f"{noun_base}{noun_group.singular_ending}"
+            case 2:
+                adj = f"{adj_base}{adj_group.neuter_ending}"
+                noun_group = random.choice(self._nouns.neuter)
+                noun_base = random.choice(noun_group.base)
+                noun = f"{noun_base}{noun_group.singular_ending}"
+            case _:
+                adj = f"{adj_base}{adj_group.plural_ending}"
+                match random.randint(0, 2):
+                    case 0:
+                        noun_group = random.choice(self._nouns.neuter)
+                        noun_base = random.choice(noun_group.base)
+                        noun = f"{noun_base}{noun_group.plural_ending}"
+                    case 1:
+                        noun_group = random.choice(self._nouns.female)
+                        noun_base = random.choice(noun_group.base)
+                        noun = f"{noun_base}{noun_group.plural_ending}"
+                    case _:
+                        noun_group = random.choice(self._nouns.female)
+                        noun_base = random.choice(noun_group.base)
+                        noun = f"{noun_base}{noun_group.plural_ending}"
+        title = f"{adj} {noun}"
+        return title if len(title) <= 16 else self.next_title()
